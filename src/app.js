71a2f69c9a9df07a6d81a523120cbc5efe99c64b
josgrehd <josgrehd@gmail.com>
2022-09-28 19:24:46 +0000

    generador de dorminio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+
+  var adj = ["great", "big"];
+
+  var noun = ["jogger", "racoon"];
+
+  let cadena1 = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        cadena1.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  console.log(cadena1);
 };
